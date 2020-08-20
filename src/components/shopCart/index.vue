4932ab535fb191c612dc1f0d08a4de3d0d20b973
lumenglong <162562xuanbao>
2020-08-20 11:11:22 +0800

    购物车结算/点击返回顶部

--- src/components/shopCart/index.vue
+++ src/components/shopCart/index.vue
@@ -2,24 +2,34 @@
   <div>
     <p class="shopTitle">我的购物车</p>
     <div style="margin-bottom: 100px;">
-    <div class="content" v-for="(item,index) in shopList" :key="index">
-          <van-card
-            :num="item.number"
-            :price="item.retail_price"
-            :title="item.goods_name"
-            :thumb="item.list_pic_url"
-          >
-            <template #footer>
-              <van-button size="mini" @click="jiesuan(item)" style="background: #CC6600;color: #ffffff;padding: 6px 6px; border-radius: 8px;border: none">结算</van-button>
-              <van-button size="mini" @click="deleteShop(item)" style="background: #CC6600;color: #ffffff;padding: 6px 6px; border-radius: 8px;border: none">删除</van-button>
-            </template>
-        </van-card>
+      <van-checkbox-group v-model="result" ref="checkboxesAll">
+        <div class="content" v-for="(item,index) in shopList" :key="index">
+              <van-card
+                :num="item.number"
+                :price="item.retail_price"
+                :title="item.goods_name"
+                :thumb="item.list_pic_url"
+                @click="toggle(index,item)"
+              >
+                <template #footer>
+    <!--              <van-button size="mini" @click="jiesuan(item)" style="background: #CC6600;color: #ffffff;padding: 6px 6px; border-radius: 8px;border: none"></van-button>-->
+                  <van-checkbox :name="item" ref="checkboxes" checked-color="#0066cc" style="position: absolute;left: 5px;top: 45px"/>
+    <!--              <van-button size="mini" @click="deleteShop(item)" style="background: #CC6600;color: #ffffff;padding: 6px 6px; border-radius: 8px;border: none">删除</van-button>-->
+                </template>
+            </van-card>
+        </div>
+        <van-submit-bar button-text="去结算"  @submit="onSubmit" v-if="result.length != 0">
+            <van-button  @click="quanxuan" style="border: none" v-if="allcheck">全选</van-button>
+          <van-button  @click="Qxquanxuan" style="border: none" v-else>取消</van-button>
+            <span style="width: 3rem;margin-left: 50px;color: #cc6600">总计:￥{{totalPrice}}</span>
+        </van-submit-bar>
+        <van-submit-bar button-text="去结算"  @submit="onSubmit" disabled v-else>
+          <van-button  @click="quanxuan" style="border: none" v-if="allcheck">全选</van-button>
+          <van-button  @click="Qxquanxuan" style="border: none" v-else>取消</van-button>
+          <span style="width: 3rem;margin-left: 50px;color: #cc6600">总计:￥{{totalPrice}}</span>
+        </van-submit-bar>
+      </van-checkbox-group>
     </div>
-    </div>
-    <van-submit-bar button-text="去结算"  @submit="onSubmit">
-        <van-checkbox>全选</van-checkbox>
-        <span style="width: 3rem;margin-left: 50px">总计:{{totalPrice}}</span>
-    </van-submit-bar>
     <foot-nav></foot-nav>
   </div>
 </template>
@@ -35,15 +45,27 @@
           openId: 'oQmbb4sNZdxaUQZ0sfYgvtOP2S7c',
           id: '',
         },
+        result: [],
         //总价
-        totalPrice: '',
+        // totalPrice: '',
+        //显示全选全部选
+        allcheck: true,
       }
     },
     components:{
       footNav,
     },
     computed:{
-
+      totalPrice: function () {
+        var total =0;
+        if (this.result.length != 0) {
+        for(var i = 0;i<this.result.length;i++){
+          var item = this.result[i];
+          total +=item.retail_price*item.number;
+        }
+        }
+        return total.toString().replace(/\B(?=(\d{3})+$)/g,',');
+      }
     },
     methods:{
       //查询购物车
@@ -70,10 +92,24 @@
       },
       //点击结算
       jiesuan(item){
+
       },
       // 去结算
       onSubmit() {
         this.$router.push('/orderDetails')
+      },
+      //选中商品
+      toggle(index,item) {
+        this.$refs.checkboxes[index].toggle();
+      },
+      //全选商品
+      quanxuan() {
+        this.allcheck = false;
+        this.$refs.checkboxesAll.toggleAll(true);
+      },
+      Qxquanxuan() {
+        this.allcheck = true;
+        this.$refs.checkboxesAll.toggleAll(false);
       }
     },
     created() {
